cae2ca1f5cf0c5e5e374f95870dd73e51cbb0960
Андрей Коваленко <mr.chrisis112@gmail.com>
2024-03-15 19:48:50 +0200

    first commit

--- src/components/menu/AddToCartButton.js
+++ src/components/menu/AddToCartButton.js
@@ -1,4 +1,3 @@
-import FlyingButton from 'react-flying-item'
 
 export default function AddToCartButton({
   hasSizesOrExtras, onClick, basePrice, image
@@ -6,14 +5,14 @@ export default function AddToCartButton({
   if (!hasSizesOrExtras) {
     return (
       <div className="flying-button-parent mt-4">
-        <FlyingButton
+        <button
           targetTop={'5%'}
           targetLeft={'95%'}
           src={image}>
           <div onClick={onClick}>
             Add to cart €{basePrice}
           </div>
-        </FlyingButton>
+        </button>
       </div>
     );
   }
