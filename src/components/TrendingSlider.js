c39aa1d5a12438f7597678f4de106320a5cbce02
Андрей Коваленко <mr.chrisis112@gmail.com>
2024-03-25 01:01:15 +0200

    fixes

--- src/components/TrendingSlider.js
+++ src/components/TrendingSlider.js
@@ -15,7 +15,7 @@ const TrendingSlider = () => {
         }}   src="https://i.imgur.com/7lpoEiL.png"></img></span>
       <span><img style={{
         paddingLeft: "20px",
-        minWidth: "500px"  }} 
+        minWidth: "550px"  }} 
         src="https://i.imgur.com/sCvoOA4.png"></img></span>
       <span><img style={{
         minWidth: "500px"}}  id= "backBtn"src="https://i.imgur.com/j58q4JM.png"></img></span>
