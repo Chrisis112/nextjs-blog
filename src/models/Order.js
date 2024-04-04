94ec316475a96932a1647339af846614832ef290
Andrei Kovalenko <71775011+Chrisis112@users.noreply.github.com>
2024-04-04 21:56:07 +0300

    Update Order.js

--- src/models/Order.js
+++ src/models/Order.js
@@ -1,13 +1,11 @@
 import {model, models, Schema} from "mongoose";
 
 const OrderSchema = new Schema({
-  userEmail: String,
-  phone: String,
-  streetAddress: String,
-  postalCode: String,
-  city: String,
-  country: String,
-  cartProducts: Object,
+  uuserEmail: {String, required: false},
+  phone: {String, required: false},
+  streetAddress: {String, required: false},
+  postalCode: {String, required: false},
+  cartProducts: Object, required: false,
   orderNumber: {type:Number, required: true},
   paid: {type: Boolean, default: false},
   orderPoints: {type:Number, required: false},
