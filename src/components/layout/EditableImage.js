e506786c3b5164bd791f1493a7b178a7fde8795c
Andrei Kovalenko <71775011+Chrisis112@users.noreply.github.com>
2024-09-15 18:54:45 +0300

    Update EditableImage.js

--- src/components/layout/EditableImage.js
+++ src/components/layout/EditableImage.js
@@ -36,13 +36,15 @@ export default function EditableImage({link, setLink}) {
       )}
       {!link && (
         <div className="text-center bg-gray-200 p-4 text-gray-500 rounded-lg mb-1">
-          No image
+          No image 
+          <h1>for best image quality pleace use photo resalution 2848 x 4288</h1>
         </div>
-       <h1>for best image quality pleace use photo resalution 2848 x 4288</h1>
+        
       )}
       <label>
         <input type="file" className="hidden" onChange={handleFileChange} />
-        <span className="block border border-gray-300 rounded-lg p-2 text-center cursor-pointer">Change image</span>
+        <span className="block border border-gray-300 rounded-lg p-2 text-center cursor-pointer">Change image  </span>
+        
       </label>
     </>
   );
