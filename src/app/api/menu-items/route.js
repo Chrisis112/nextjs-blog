7f5f6810a6487fd3ceddeb3189009df35ff29d9c
Андрей Коваленко <mr.chrisis112@gmail.com>
2025-10-06 00:13:17 +0300

    Переводы и уведомления3

--- src/app/api/menu-items/route.js
+++ src/app/api/menu-items/route.js
@@ -1,8 +1,7 @@
-import {isAdmin} from "@/app/api/libs/auth";
-import {MenuItem} from "@/models/MenuItem";
+import { isAdmin } from "@/app/api/libs/auth";
+import { MenuItem } from "@/models/MenuItem";
 import mongoose from "mongoose";
 
-// Помощник для конвертации строки в объект переводов
 function normalizeMultilangFields(data) {
   if (typeof data.name === 'string') {
     data.name = { ru: data.name, en: '', et: '' };
@@ -14,10 +13,12 @@ function normalizeMultilangFields(data) {
 }
 
 export async function POST(req) {
-  mongoose.connect(process.env.MONGO_URL);
+  if (mongoose.connection.readyState !== 1) {
+    await mongoose.connect(process.env.MONGO_URL);
+  }
   let data = await req.json();
   if (await isAdmin()) {
-    data = normalizeMultilangFields(data); // конвертация перед сохранением
+    data = normalizeMultilangFields(data);
     const menuItemDoc = await MenuItem.create(data);
     return Response.json(menuItemDoc);
   } else {
@@ -26,26 +27,32 @@ export async function POST(req) {
 }
 
 export async function PUT(req) {
-  mongoose.connect(process.env.MONGO_URL);
+  if (mongoose.connection.readyState !== 1) {
+    await mongoose.connect(process.env.MONGO_URL);
+  }
   if (await isAdmin()) {
-    const {_id, ...dataRaw} = await req.json();
-    const data = normalizeMultilangFields(dataRaw); // конвертация перед обновлением
+    const { _id, ...dataRaw } = await req.json();
+    const data = normalizeMultilangFields(dataRaw);
     await MenuItem.findByIdAndUpdate(_id, data);
   }
   return Response.json(true);
 }
 
 export async function GET() {
-  mongoose.connect(process.env.MONGO_URL);
+  if (mongoose.connection.readyState !== 1) {
+    await mongoose.connect(process.env.MONGO_URL);
+  }
   return Response.json(await MenuItem.find());
 }
 
 export async function DELETE(req) {
-  mongoose.connect(process.env.MONGO_URL);
+  if (mongoose.connection.readyState !== 1) {
+    await mongoose.connect(process.env.MONGO_URL);
+  }
   const url = new URL(req.url);
   const _id = url.searchParams.get('_id');
   if (await isAdmin()) {
-    await MenuItem.deleteOne({_id});
+    await MenuItem.deleteOne({ _id });
   }
   return Response.json(true);
 }
